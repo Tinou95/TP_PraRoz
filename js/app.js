e1ea41587459bd97f413757672d58048ca177676
Quentin <72099011+Tinou95@users.noreply.github.com>
2022-04-06 17:23:17 +0200

    thirdCommit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () =>{
+    const element = document.querySelector("main form .warning");
+    const email = document.querySelector(".email");
+    const password = document.querySelector(".password");
+    const submit = document.querySelector(".submit");
+
+    
+	let envoieUser = () => {
+	
+		if (!email || !password ) {
+			element.innerText = "Veuillez remplir tous les champs ";
+            sessionStorage.clear();
+		 
+		} else {
+			element.innerText = "Votre formulaire a bien été envoyé";
+			localStorage.setItem("user", email, "userpassword", password);
+			sessionStorage.setItem("user", "sessionid", "userpassword", "passwordid");
+
+		}
+	};
+    submit.addEventListener("click", e => {
+		e.preventDefault();
+		envoieUser();
+	});
     
 })
